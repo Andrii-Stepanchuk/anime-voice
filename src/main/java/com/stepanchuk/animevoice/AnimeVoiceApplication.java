bd9cc5c34ca9642a40626c5c07f9f1e68206c2fa
Andrii Stepanchuk <andrii.stepanchuk11@gmail.com>
2023-06-04 19:59:52 +0300

    init project

---
+++ src/main/java/com/stepanchuk/animevoice/AnimeVoiceApplication.java
@@ -0,0 +1,13 @@
+package com.stepanchuk.animevoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeVoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeVoiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stepanchuk/animevoice/AnimeVoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stepanchuk.animevoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeVoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
